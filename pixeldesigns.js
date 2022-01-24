44c3a1dfb9fae64b99a410beaa87d53f73ac1395
Vijay Paul <vijay.pauln@gmail.com>
2022-01-24 11:47:29 +0100

    First commit

---
+++ pixeldesigns.js
@@ -0,0 +1,37 @@
+// Select color input
+
+
+// Select size input
+
+var height,width, color;
+// When size is submitted by the user, call makeGrid()
+//creating the grid
+$('#sizePicker').submit(function(event) {
+    event.preventDefault();
+    height = $('#inputHeight').val();
+    width = $('#inputWidth').val();
+  makeGrid(height,width);
+  console.log('height:'+ height+'  width:'+ width);
+})
+
+function makeGrid(x,y){
+  $('tr').remove();
+
+// Your code goes here!
+for (var i=1;i<=x;i++){
+  $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+  for(var j = 1;j<=y;j++){
+    $('#table' + i).append('<td></td>');
+  }
+}
+//add color to the cell
+$('td').click(function addColor(){
+  color = $('#colorPicker').val();
+
+  if ($(this).attr('style')){
+    $(this).removeAttr('style')
+  } else{
+    $(this).attr('style', 'background-color:' + color);
+  }
+})
+}
