335bd6c932ae979671ce52f151166a30ee147866
vijaynpaul <61095605+vijaynpaul@users.noreply.github.com>
2022-01-28 17:53:50 +0100

    Add files via upload

--- pixeldesigns.js
+++ pixeldesigns.js
@@ -9,52 +9,47 @@ const submit = document.getElementById('submitbutton')
 // Your code goes here!
 // When size is submitted by the user, call makeGrid()
 
+submit.addEventListener("click", function(event) {
+	event.preventDefault();
+	event.stopPropagation();
+	let row = document.getElementById('inputHeight').value;
+	let column = document.getElementById('inputWidth').value;
+	makeGrid(row, column);
+	console.log('height:' + row + '  width:' + column);
+});
 
 
 
-submit.addEventListener("click", function(event)  {
-  event.preventDefault();
-  event.stopPropagation();
-  let row = document.getElementById('inputHeight').value;
-  let column = document.getElementById('inputWidth').value;
-makeGrid(row,column);
-console.log('height:'+ row +'  width:'+column);
-}); 
-
-
-
-//creating the grid with the input values 
-function makeGrid(row,column){
-  canvas.innerHTML='';
-for(var i = 0; i < row; i++){
-  let canvasRow = canvas.insertRow(i);  
-  for(var j = 0;j < column;j++){
-    canvasRow.insertCell(j);
-  }
- }
+//Creating the grid with the values from the user
+function makeGrid(row, column) {
+	canvas.innerHTML = '';
+	for (var i = 0; i < row; i++) {
+		let canvasRow = canvas.insertRow(i);
+		for (var j = 0; j < column; j++) {
+			canvasRow.insertCell(j);
+		}
+	}
 
 }
 console.log(canvas);
 
-document.getElementById('colorPicker').addEventListener("change", function(event){
-  event.preventDefault();
-  colorEl =this.value;
-  console.log(colorEl); 
- 
-}
-) ;
+document.getElementById('colorPicker').addEventListener("change", function(event) {
+	event.preventDefault();
+	colorEl = this.value;
+	console.log(colorEl);
+
+});
 
 
 canvas.addEventListener('click', function(e){
-e.stopPropagation();
-let td = e.target;
-// unclick the color cell
-if (td.hasAttribute("style")){
-  td.removeAttribute("style");}
-else{
+  e.stopPropagation();
+  let td = e.target;
+  // unclick the color cell
+  if (td.style.backgroundColor == colorEl) {
+  } else {
   td.style.backgroundColor = colorEl;
-}
+  }
+  
+  console.log(td.style);
+  });
 
-
-console.log(td.style);
-});
